441770b5cf3e9d3bdb7e0aa2afa4ff7ac5ebc5d7
Sneha Vijayarajan <sneha.vijayarajan@gmail.com>
2021-03-23 16:30:53 +0530

    Add test class path

--- hadoop-tools/hadoop-azure/runE2ETest.sh
+++ hadoop-tools/hadoop-azure/runE2ETest.sh
@@ -1,2 +1,3 @@
+echo "Test path: src/test/java/org/apache/hadoop/fs/azurebfs/services/ITestAbfsFastpath.java"
 mvn test -Dtest=org.apache.hadoop.fs.azurebfs.services.ITestAbfsFastpath 
 cat target/surefire-reports/org.apache.hadoop.fs.azurebfs.services.ITestAbfsFastpath-output.txt
